fc81b560c820e4278624861743413c400620a835
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-02-25 18:42:58 +0200

    CUB-P1 add constants file, extract  express and handlebars configurations

--- 02. ExpressJS and Templating/Workshop Cubicle/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ 02. ExpressJS and Templating/Workshop Cubicle/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,30 +1,20 @@
-const express = require('express');
-const handlebars = require("express-handlebars");
-const path = require('path')
+// Imports
+const express = require("express");
+const handlebarsConfig = require("./config/handlebarsConfig");
+const expressConfig = require("./config/expressConfig");
+const { PORT } = require("./constants");
 
-const app = express()
-const PORT = 5050;
+// Locale variables
+const app = express();
 
-// Handlebars config
-app.engine("hbs", handlebars.engine({ extname: "hbs"}));
-app.set('view engine', 'hbs');
-app.set("views", 'src/views');
+// Configs
+expressConfig(app);
+handlebarsConfig(app);
 
-// Setup static files
-
-//app.use(express.static("src/public"))
-// or
-//const staticFiles = express.static(path.resolve(__dirname, "public"));
-// app.use(staticFiles())
-
-app.use(express.static(path.resolve(__dirname, "public")));
-
+// Routing
 app.get("/", (req, res) => {
-    // res.send("Hello from express server!")
-
-    res.render("index", )
+  // res.send("Hello from express server!")
+  res.render("index");
 });
-
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
