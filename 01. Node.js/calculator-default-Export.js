886b1b8ae9acad316ffcc3e7de7e71bfac621e05
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-02-25 16:22:42 +0200

    Initial commit

---
+++ 01. Node.js/calculator-default-Export.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const calculator = {
+    sum,
+    substract,
+    multiply,
+    divide,
+};
+
+// default
+
+module.exports = calculator;
---
+++ 01. Node.js/index.js
@@ -0,0 +1,7 @@
+const calculateDefaultExport = require("./calculator-default-Export")
+const calculateNamedExport = require("./calculator-named-Export")
+
+console.log(calculateDefaultExport.sum(3, 4));
+console.log(calculateNamedExport.sum(3, 4));
+
+
